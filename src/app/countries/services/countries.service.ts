f3a227e368eff762c614bbbd38f1eb38fdadca39
Jona <=>
2023-05-27 12:37:03 -0600

    Agrega pages y components

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap, map, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -10,11 +10,54 @@ private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) { }
 
+  searchCountryByAlphaCode( code:  string ): Observable<Country[]> {
+    const url = `${ this.apiUrl }/aplha/${ code }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of([]) )
+      )
+
+  }
+
   searchCapital( term: string ): Observable<Country[]> {
 
     const url = `${ this.apiUrl }/capital/${ term }`;
 
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url )
+      .pipe(
+        // tap( countries => console.log('Por tap1', countries) ),
+        // map( countries => [] ),
+        // tap( countries => console.log('Por tap2', countries) ),
+        // of - Construye un observable en base al argumento que recibe
+        catchError( error => {
+          // console.log(error);
+
+          return of([]);
+        })
+
+        // Opciones válidas si no se ocupa el error
+        // catchError( (error) => of([]) )
+        // catchError( () => of([]) )
+      );
+  }
+
+  searchCountry( term: string ): Observable<Country[]> {
+    const url = `${ this.apiUrl }/name/${ term }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of([]) )
+      )
+  }
+
+  searchRegion( region: string ): Observable<Country[]> {
+    const url = `${ this.apiUrl }/region/${ region }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of([]) )
+      )
   }
 
 }
